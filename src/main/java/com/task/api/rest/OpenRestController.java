da87c4b694302bbbccc62d826b8b0f9eb30186f9
Bruno Ribeiro <bruno.marshall22@gmail.com>
2025-03-19 09:25:17 -0300

    limpando imports

--- src/main/java/com/task/api/rest/OpenRestController.java
+++ src/main/java/com/task/api/rest/OpenRestController.java
@@ -1,14 +1,6 @@
 package com.task.api.rest;
 
-import com.task.api.model.User;
-import com.task.api.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDateTime;
 
 @RestController
